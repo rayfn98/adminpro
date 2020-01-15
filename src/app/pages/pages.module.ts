950f690bb3a6d3cf1086fbd6f4d1b88f71ff9240
Ray Flores Nolasco <rayfn98@gmail.com>
2020-01-15 10:42:54 -0500

    Seccion 4 terminada

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { sharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        sharedModule,
+        PAGES_ROUTES
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+
+    ]
+})
+export class PagesModule { }
